7d2964c5832c7be09ff6958e1c59aff8f98b51be
Mauri <mauri.monzon91@gmail.com>
2024-03-29 18:40:15 -0300

    Icono flotante

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
-import { SafeAreaView } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import { CounterScreen } from './src/presentation/screens/CounterScreen';
+import {SafeAreaView} from 'react-native';
+import {PaperProvider} from 'react-native-paper';
+import {CounterScreen} from './src/presentation/screens/CounterScreen';
+import {CounterM3Screen} from './src/presentation/screens/CounterM3Screen';
 
 export const App = () => {
   return (
     <PaperProvider>
       <SafeAreaView style={{flex: 1}}>
         {/* <HelloWorldScreen name="Mauricio Monzón" /> */}
-        <CounterScreen />
+        {/*  <CounterScreen /> */}
+        <CounterM3Screen />
       </SafeAreaView>
     </PaperProvider>
   );
