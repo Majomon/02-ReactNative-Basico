d36b4ca3c35d187dbf52c2023637778fe2c682e4
Mauri <mauri.monzon91@gmail.com>
2024-04-01 20:58:50 -0300

    a

--- src/presentation/screens/PositionScreen.tsx
+++ src/presentation/screens/PositionScreen.tsx
@@ -3,25 +3,37 @@ import {StyleSheet, Text, View} from 'react-native';
 export const PositionScreen = () => {
   return (
     <View style={style.container}>
-    {/*   <Text style={style.title}>Hola mundo!!!</Text> */}
-    <View style={style.purpleBox}/>
+      {/*   <Text style={style.title}>Hola mundo!!!</Text> */}
+      <View style={style.purpleBox} />
+      <View style={style.orangeBox} />
     </View>
   );
 };
 
 const style = StyleSheet.create({
   container: {
-    flex: 1,
+/*   */
     backgroundColor: '#28C4D9',
+    justifyContent:"center",
+    alignItems:"center"
   },
   title: {
     color: 'black',
   },
-  purpleBox:{
-    width:100,
-    height:100,
-    backgroundColor:"#5856D6",
-    borderWidth:10,
-    borderColor:"white"
-  }
+  purpleBox: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#5856D6',
+    borderWidth: 10,
+    borderColor: 'white',
+  },
+  orangeBox: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#F0A23B',
+    borderWidth: 10,
+    borderColor: 'white',
+    top:0,
+    left:50
+  },
 });
